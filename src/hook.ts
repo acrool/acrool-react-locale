0436cdebc6995ee2058590de9798863ddd00ba5c
imagine10255 <imagine10255@gmail.com>
2022-02-26 15:52:58 +0800

    fix: fix useLocale type

--- src/hook.ts
+++ src/hook.ts
@@ -1,14 +1,14 @@
 import {useIntl} from 'react-intl';
 import {useCallback, useContext} from 'react';
 import {LocaleContext} from './LocaleProvider/context';
-import {TI18n} from 'typings';
+import {TTranslateI18n} from 'typings';
 
 
 export const useLocale = () => {
     const {locale, setLocale} = useContext(LocaleContext);
     const {formatMessage} = useIntl();
 
-    const i18n: TI18n = useCallback((id, options) => {
+    const i18n: TTranslateI18n = useCallback((id, options) => {
         return formatMessage({id, defaultMessage: options?.defaultMessage}, options?.params);
     }, [locale]);
 
