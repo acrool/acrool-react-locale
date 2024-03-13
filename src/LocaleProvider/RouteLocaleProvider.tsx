836498f4daa3d35b0c0d37d3e2319b025afe0e57
imagine10255 <imagine10255@gmail.com>
2024-03-13 10:56:03 +0800

    add RouteLocaleProvider

---
+++ src/LocaleProvider/RouteLocaleProvider.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import React, {Children, useEffect, useState} from 'react';
+import LocaleProvider from './LocaleProvider';
+import {TLocale, TLocaleDictionaries} from '../types';
+
+
+
+interface IProps{
+    localeDictionaries: TLocaleDictionaries
+    children: JSX.Element
+    defaultLocale: TLocale,
+    currentLocale: string,
+}
+
+
+const RouteLocaleProvider = ({
+    localeDictionaries,
+    currentLocale,
+    defaultLocale,
+    children
+}: IProps) => {
+    const initLocale = (currentLocale || defaultLocale) as TLocale;
+    const [locale, setLocale] = useState<TLocale>(initLocale);
+
+    return <LocaleProvider
+        locale={locale}
+        setLocale={setLocale}
+        defaultLocale={defaultLocale}
+        localeDictionaries={localeDictionaries}
+    >
+        {Children.only(children)}
+    </LocaleProvider>;
+};
+
+export default RouteLocaleProvider;
