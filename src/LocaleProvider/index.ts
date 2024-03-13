628bbba04eb1de06b18a6a9eba140470f12009d5
imagine10255 <imagine10255@gmail.com>
2024-03-13 10:58:17 +0800

    add export RouteLocaleProvider

--- src/LocaleProvider/index.ts
+++ src/LocaleProvider/index.ts
@@ -1,3 +1,4 @@
 export {default} from './LocaleProvider';
 export {default as StateControlLocaleProvider} from './StateControlLocaleProvider';
+export {default as RouteLocaleProvider} from './RouteLocaleProvider';
 export {LocaleContextConsumer} from './context';
