2640577acb5aaec0ed7a4aea304db59631958bac
imagine10255 <imagine10255@gmail.com>
2024-06-25 15:42:33 +0800

    modify READEME

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -10,16 +10,27 @@ import {DEFAULT_LOCALE, localeDictionaries} from './config/locale';
 function App() {
 
 
-    return (
-        <div className="App">
-            <a href="https://github.com/acrool/acrool-react-locale" target="_blank" rel="noopener noreferrer">
+    const renderHeader = () => {
+
+        const repositoryUrl = 'https://github.com/acrool/acrool-react-locale';
+        const name = 'Acrool React Locale';
+
+        return <>
+            <a href={repositoryUrl} target="_blank" rel="noopener noreferrer">
                 <Github width={40} height={40}/>
             </a>
 
-            <div style={{textAlign: 'center', border: '1px solid #606060', display: 'flex', flexDirection: 'column', padding: '20px', margin: '20px 0'}}>
-                <img src="/logo.svg" style={{height: '100px'}} alt="Acrool React Locale"/>
-                <div style={{fontSize: '40px', color: '#fff', fontWeight: 700}}>Acrool React Locale</div>
+            <div className="banner-wrapper">
+                <img src="/logo.svg" alt={name}/>
+                <h1>{name}</h1>
             </div>
+        </>;
+    };
+
+
+    return (
+        <div className="App">
+            {renderHeader()}
 
             <StateControlLocaleProvider
                 localeDictionaries={localeDictionaries}
