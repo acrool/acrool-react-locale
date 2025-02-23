0105bf244eb13e95745d6db669fd7d8f5dc4d5d0
imagine10255 <imagine10255@gmail.com>
2025-02-23 11:57:29 +0800

    modify t is ignore defaultMessage

--- src/hook.ts
+++ src/hook.ts
@@ -4,11 +4,27 @@ import {LocaleContext} from './LocaleProvider/context';
 import {II18nTexts} from './types';
 
 
+/**
+ * 翻譯 Hook
+ */
 export const useLocale = <T extends II18nTexts>() => {
     const {locale, setLocale} = useContext(LocaleContext);
     const {formatMessage} = useIntl();
 
+    /**
+     * 省略
+     */
     const t = useCallback(<K extends keyof T>(
+        id: K,
+        args?: Record<string, any>,
+    ) => {
+        return formatMessage({id: id as string}, args);
+    }, [locale]);
+
+    /**
+     * 包含default方法
+     */
+    const t18n = useCallback(<K extends keyof T>(
         id: K,
         options?: { def?: string; args?: Record<string, any> }
     ) => {
@@ -18,6 +34,7 @@ export const useLocale = <T extends II18nTexts>() => {
 
     return {
         t,
+        t18n,
         locale,
         setLocale,
     };
