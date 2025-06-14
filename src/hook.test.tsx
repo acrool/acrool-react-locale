a5e960f43be9b2b9b1cdf7054cc1c4030f6cab3e
imagine <imagine10255@gmail.com>
2025-06-14 13:06:52 +0800

    Refactor `useLocale` tests to utilize `LocaleProvider` for improved readability and maintainability

--- src/hook.test.tsx
+++ src/hook.test.tsx
@@ -1,46 +1,57 @@
 import {act,renderHook} from '@testing-library/react';
-import React from 'react';
+import React, {ReactNode} from 'react';
 import {createIntl, createIntlCache, IntlProvider, RawIntlProvider} from 'react-intl';
 
 import {useLocale} from './hook';
 import {LocaleContext} from './LocaleProvider/context';
+import LocaleProvider from "./LocaleProvider";
 
-const messages = {
-    'common.hello': '你好',
-    'common.greet': '哈囉, {name}!'
+
+const localeDictionaries = {
+    'zh-TW': {
+        common: {
+            'common.hello': '你好',
+            'common.greet': '哈囉, {name}!'
+        }
+    },
+    'en-US': {
+        common: {
+            'common.hello': 'Hi',
+            'common.greet': 'Hello, {name}!'
+        }
+    }
 };
 
 describe('useLocale', () => {
-    function wrapper({children, locale = 'zh-TW'}: any) {
-        const [currentLocale, setCurrentLocale] = React.useState(locale);
-        const cache = createIntlCache();
-        const intl = createIntl({locale: currentLocale, messages, defaultLocale: 'zh-TW'}, cache);
+    function wrapper({children}: { children: ReactNode }) {
         return (
-            <LocaleContext.Provider value={{locale: currentLocale, setLocale: setCurrentLocale}}>
-                <RawIntlProvider value={intl}>
-                    {children}
-                </RawIntlProvider>
-            </LocaleContext.Provider>
+            <LocaleProvider
+                localeDictionaries={localeDictionaries}
+                defaultLocale="zh-TW"
+                ignoreMissingLocaleMessage
+            >
+                {children}
+            </LocaleProvider>
         );
     }
 
-    // it('能正確取得 t/i18n/locale/setLocale', () => {
-    //     const {result} = renderHook(() => useLocale<typeof messages>(), {wrapper});
-    //     expect(result.current.locale).toBe('zh-TW');
-    //     expect(result.current.t('common.hello')).toBe('你好');
-    //     expect(result.current.t('common.greet', {args: {name: '小明'}})).toBe('哈囉, 小明!');
-    // });
-    //
-    // it('setLocale 能切換語言', () => {
-    //     const {result} = renderHook(() => useLocale<typeof messages>(), {wrapper});
-    //     act(() => {
-    //         result.current.setLocale('en-US');
-    //     });
-    //     expect(result.current.locale).toBe('en-US');
-    // });
-    //
-    // it('t 支援 def 預設值', () => {
-    //     const {result} = renderHook(() => useLocale<typeof messages>(), {wrapper});
-    //     expect(result.current.t('common.not_exist' as any, {def: '預設值'})).toBe('預設值');
-    // });
+    it('能正確取得 t/i18n/locale/setLocale', () => {
+        const {result} = renderHook(() => useLocale(), {wrapper});
+        expect(result.current.locale).toBe('zh-TW');
+        expect(result.current.t('common.hello')).toBe('你好');
+        expect(result.current.t('common.greet', {args: {name: '小明'}})).toBe('哈囉, 小明!');
+    });
+
+    it('setLocale 能切換語言', () => {
+        const {result} = renderHook(() => useLocale(), {wrapper});
+        act(() => {
+            result.current.setLocale('en-US');
+        });
+        expect(result.current.locale).toBe('en-US');
+    });
+
+    it('t 支援 def 預設值', () => {
+        const {result} = renderHook(() => useLocale(), {wrapper});
+        expect(result.current.t('common.not_exist' as any, {def: '預設值'})).toBe('預設值');
+    });
 }); 
